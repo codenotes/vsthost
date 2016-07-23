54005095e51510d2e3f346c4b44b4e08d9233259
greg brill <gbrill@infusion.com>
2016-07-23 19:58:48 -0400

    updated

--- vsthost/include/vsthost.h
+++ vsthost/include/vsthost.h
@@ -5,8 +5,18 @@ extern "C"
 
 	void MIDIDirect(unsigned char * msg, int size, bool isSysex);
 	void setPreset(int preset, int midichannel, bool clearChannel);
+	void PlayMidiFile(char * midiFileName, unsigned char * channels, int num_channels);
 	
+	//conveiniance methods, these just create midi sysex messages at end of day
+	void setPreset2(int preset, int midichannel, bool clearChannel, int soundFontID);
+	void loadSoundFont(char * filename);
+	void setGain(int gain);
 	
 
 
 }
+
+enum VSTHostProps
+{
+	
+};
